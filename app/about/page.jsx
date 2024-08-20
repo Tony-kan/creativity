b4337f28acc47e94ee1610bb7e32745afcaf460b
Don Chatha <donchatha04@gmail.com>
2024-08-20 16:24:26 +0200

    icons of footer

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+
+const page = () => {
+  return (
+   <div className="   " >
+    <div className="relative z-10 justify- items-center container">
+      <h1 className="uppercase font-bold">  Our Vision</h1>
+      <Image src="/assets/images/slider1.jpg" alt="image" width="400" height="200"/>
+    </div>
+   </div>
+  
+  )
+}
+
+export default page 
--- components/common/footer.jsx
+++ components/common/footer.jsx
@@ -1,10 +1,10 @@
 
-import { PhoneCall ,Mail,BookOpenText,Binoculars,Bean ,MapPin,Tractor } from 'lucide-react';
+import { PhoneCall ,Mail,BookOpenText,Binoculars,Bean ,MapPin,ArrowBigRight} from 'lucide-react';
 import Link from 'next/link';
 
 const footer = () => {
   return (
-    <div  className="bg-zinc-500">
+    <div  className="bg-zinc-500 ">
     <div className=' flex flex-row gap-4 justify-between p-2  mt-5'>
             <div className="p-4">
               <div className='text-white font-semibold text-lg underline pr-16'>
@@ -34,7 +34,7 @@ const footer = () => {
                 <Mail  className="mt-12 "/>
                 </div>
                 <h1 className='text-white  pl-4'>
-                     TPIN: 30954285<br></br>
+                    TPIN: 30954285<br></br>
                     P.O. Box 1493,<br></br> Lilongwe,<br></br>
                     P.O. Box 59,<br></br> Lumbadzi,<br></br> Malawi<br></br>
                     Phones:<br></br> 
@@ -44,38 +44,29 @@ const footer = () => {
                                     </h1>
               </div>
             </div>
-            {/* <div>
-              <div className='text-white font-semibold underline p mt-5'>
-                Quick Links
-              </div>
-              <div>
-                <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br></br> 
-                  Mollitia fuga commodi reiciendis facilis <br></br>
-                  possimus voluptas nam explicabo necessitatibus, <br></br>
-                  incidunt ab fugit corporis numquam totam. <br></br>
-                  Reiciendis modi dicta voluptates ullam recusandae!
-                  </h1>
-              </div>
-            </div> */}
-            <div className="px-4">
-              <div className='text-white  font-semibold underline pr-16  mt-5'>
+            <div className="">
+              <div className='text-white  font-semibold underline pl-16  mt-5'>
                 Our Services
               </div>
-              <div className="">
-              <div className=" justify-center items-center">
-               <Link href="/" className="font-semibold text-white ">   <Tractor className="text-black" />Multiplication of Quality Seeds</Link>
-              </div>
-              <div>
-               <Link href="/" className="font-semibold text-white">   <Bean className="text-black"/> Supply quality Seeds</Link>
-              </div>
-              <div>
-               <Link href="/"className="font-semibold text-white">   <BookOpenText className="text-black" /> Farmers Consultation</Link>
-              </div>
-              <div>
-               <Link href="/"className="font-semibold text-white">  <Binoculars className="text-black"/> Conduct Researches</Link>
-              </div>
+              <div  className='flex flex-row justify-center relative '>
+              <div className=" className='text-white  ">
+              <div className="flex  justify-center items-center">
+                <div className="flex flex-col">
+              <ArrowBigRight className=""/>
+              <ArrowBigRight className="mt-1"/>
+              <ArrowBigRight className="mt-2"/>
+              <ArrowBigRight className="mt-4"/>
+                </div>
+                <h1  className='text-white  pl-6 '>
+                <Link href="Muitiplication of Quality Seeds">Multiplication of Quality Seeds </Link> <br></br>
+                 <Link href="Supply quality Seeds">Supply quality Seeds</Link> <br></br>
+                 <Link href="Farmers Consultation">Farmers Consultation</Link> <br></br>
+                 <Link href="Conduct Researches">Conduct Researches</Link> <br></br>
+                </h1>
+            </div>
             </div>
               </div>
+       </div>
     </div>
     <div className="inline-flex items-center justify-center w-full">
     <hr className="w-80 h-1 my-2 bg-gray-200 border-0 rounded dark:bg-gray-700"/>
